f57fb8212d96a722f25ead86eef4f382f54bef14
JaviMorGab <franjamoga.93@gmail.com>
2021-10-01 14:01:17 +0200

    ruta logout corregida

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,10 @@ use App\Http\Controllers\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::middleware(['auth:sanctum'])->group(function() {
+
+Route::get('/logout', [AuthController::class, 'logout']);
+
+}
 
 
